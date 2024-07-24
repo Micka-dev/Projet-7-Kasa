0f897392c556b3ab73f55abaa957693a3b26f17f
Mickaël Douceré <mickamickadou@gmail.com>
2024-07-25 01:51:17 +0200

    Cration CSS principal + création composants Header & Footer et leurs scss + importation logo

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import Logo from '../../Assets/Logo/LOGO-white.png'
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <img className="footer-container_logo" src={Logo} alt="Logo du site" />
+      <p className="footer-container_text">© 2020 Kasa. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,11 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../../Assets/Logo/LOGO.svg'
 
 function Header() {
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/about">A Propos</Link>
-    </nav>
+    <div className="header-container main-container">
+      <img className="header-container_logo" src={Logo} alt="Logo du site" />
+
+      <nav className="header-container_nav">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? 'header-container_link-activated' : null
+          }
+          to="/"
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? 'header-container_link-activated' : null
+          }
+          to="/about"
+        >
+          A Propos
+        </NavLink>
+        {/* <Link to="/housing/bookingId">Fiche logement</Link> */}
+      </nav>
+    </div>
   )
 }
 
