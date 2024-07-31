3e7e79abb17d96abd714c5a88f4a02dc5450ce0c
Mickaël Douceré <mickamickadou@gmail.com>
2024-07-31 19:46:52 +0200

    construction page about + collapses fonctionnelles et animées

--- src/Components/Collapse/collapse.jsx
+++ src/Components/Collapse/collapse.jsx
@@ -1,11 +1,54 @@
-function Collapse({ title, content }) {
+import '../../Components/Collapse/collapse.scss'
+import { useState, useRef } from 'react'
+
+function Collapse({ data }) {
+  const [OpeningState, setOpeningState] = useState(true)
+
+  const [icon, setIcon] = useState(
+    'container-collapses_collapse_icon fa-solid fa-chevron-up'
+  )
+
+  const [dropUp, setDropUp] = useState('displaiyInitial')
+
+  const target = useRef(null)
+
+  const toggleOpeningState = () => {
+    setOpeningState(!OpeningState)
+    setIcon(
+      OpeningState
+        ? 'anticlockwiseRotationIcon container-collapses_collapse_icon fa-solid fa-chevron-up'
+        : 'clockwiseRotationIcon container-collapses_collapse_icon fa-solid fa-chevron-up'
+    )
+
+    // Supprime la balise <p> du DOM lorsque OpeningState passe à false
+    setDropUp(
+      OpeningState
+        ? ' dropDown container-collapses_content main-container'
+        : 'dropUp  container-collapses_content main-container'
+    )
+    const displayNoneDelay = setTimeout(() => {
+      !OpeningState ? target.current.classList.add('displaiyInitial') : null
+    }, 200)
+    return () => {
+      clearTimeout(displayNoneDelay)
+    }
+  }
+
   return (
-    <div>
-      <div className="collapse-container main-container">
-        <h2 className="collapse-container_title">{title}</h2>
-        <i className="collapse-container_icon fa-solid fa-chevron-up"></i>
+    <div className="container-collapses main-container">
+      <div className="container-collapses_collapse ">
+        <h2 className="container-collapses_collapse_title">{data.title}</h2>
+        <i className={icon} onClick={toggleOpeningState}></i>
       </div>
-      <p className="collapse-container_content main-container">{content}</p>
+      <p
+        ref={target}
+        className={dropUp}
+        // className={`container-collapses_content main-container ${
+        //   OpeningState ? 'dropDown' : 'dropUp'
+        // }`}
+      >
+        {data.content}
+      </p>
     </div>
   )
 }
